9786171112e4b4c43e5b103d7cbce22e40359942
Travis Medley <medley4th@gmail.com>
2020-12-28 18:38:11 -0800

    start of login

--- views.py
+++ views.py
@@ -1,30 +1,100 @@
-from flask import Flask, request, render_template
-from db_functions import *
-import sys
-
-#views from the top
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_wtf.csrf import CSRFProtect, CSRFError
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import requests as r
+import json as j
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I<+g/P2N$}0GXOf'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+# IMPORTANT - GENERATES CSRF TOKEN
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+login_manager.login_view = 'login'
 
-@app.route('/')
-def index():
-    return render_template("home.html")
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
 
-@app.route('/about')
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
+@app.route("/home", methods=['GET', 'POST'])
+@app.route("/")
+def home():
+    x = r.get("https://uselessfacts.jsph.pl/random.json?language=en")
+    data = j.loads(x.content)
+    fact = data.get("text")
+    return render_template("facts.html", fact=fact)
+
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route('/catalog')
-def catalog():
-    cats = get_cats()
-    print(cats, file=sys.stderr)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
 
-    return render_template("catalog.html", cats=cats)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
 
-# @app.route('/search')
-# def search_bar():
-#     search_query = "travis andrew"
-#     x = search(search_query)
+        return render_template('invalid.html', form=form)
+    #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('login.html', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        # redirect to page when user is created
+        return render_template('usercreatedredirect.html')
+    # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+
+    return render_template('signup.html', form=form)
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
-	app.run(debug=True) #to visit site, go to http://0.0.0.0:5000
+    app.run(debug=True)
